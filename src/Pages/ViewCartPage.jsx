8f7ed71da3ca3c7dc10dbb72ebac8620d131d204
Rihanat <bellorihanat015@gmail.com>
2026-09-09 23:15:04 +0100

    I have not done order summary yet (should be in same page with orders in viewcartpage

--- src/Pages/ViewCartPage.jsx
+++ src/Pages/ViewCartPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ViewCartPageHeader from '../SubSections/ViewCartPageSubScetions/ViewCartHeader'
 import AllOrders from '../SubSections/ViewCartPageSubScetions/AllOrders'
+import OrderSummary from '../SubSections/ViewCartPageSubScetions/OrderSummary'
 
 const ViewCartPage = () => {
   return (
     <div className=' border-2 border-green-700 min-h-[200vh]'>
       <ViewCartPageHeader/>
       <AllOrders/>
+      <OrderSummary/>
     </div>
   )
 }
